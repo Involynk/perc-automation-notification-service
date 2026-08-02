31cdf4c762b7031aff33a596107703d0c8fec621
KishanKumar <kishankumarhv5@gmail.com>
2026-08-02 11:28:37 +0530

    feat(notification): add root redirect to Swagger API docs at http://localhost:3004/api/docs

--- packages/notification-service/src/app.module.ts
+++ packages/notification-service/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { PrismaService } from './prisma/prisma.service';
 import { NotificationModule } from './notification/notification.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { NotificationModule } from './notification/notification.module';
     EventEmitterModule.forRoot(),
     NotificationModule,
   ],
+  controllers: [AppController],
   providers: [PrismaService],
   exports: [PrismaService],
 })
